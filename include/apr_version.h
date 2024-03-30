4023c77d2300b4497fbc94b12a86734a760d42b6
Rainer Jung <rjung@apache.org>
2024-03-30 21:15:44 +0000

    Adjust copyright notice to the form used by the web server as suggested by Roy
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1916671 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_version.h
+++ include/apr_version.h
@@ -38,8 +38,7 @@
  */
 
 
-#define APR_COPYRIGHT "Copyright (c) 2000-2024 The Apache Software " \
-                      "Foundation or its licensors, as applicable."
+#define APR_COPYRIGHT "Copyright 2024 The Apache Software Foundation."
 
 /* The numeric compile-time version constants. These constants are the
  * authoritative version numbers for APR.
